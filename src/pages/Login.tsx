392436e1958596da655c7fd7d97a43e81ec04c98
UttkarshZtw <121866885+UttkarshZtw@users.noreply.github.com>
2024-07-28 14:16:40 +0530

    Changed to theme color at all places

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import LoginForm from '../components/Auth/LoginForm'
 
 const Login: React.FC = () => {
   return (
-    <div className='h-screen flex items-center justify-center bg-gray-900'>
+    <div className='h-screen flex items-center justify-center'>
       <LoginForm />
     </div>
   )
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,7 +3,7 @@ import RegisterForm from '../components/Auth/RegisterForm'
 
 const Register: React.FC = () => {
   return (
-    <div className='h-screen flex items-center justify-center bg-gray-900'>
+    <div className='h-screen flex items-center justify-center'>
       <RegisterForm />
     </div>
   )
